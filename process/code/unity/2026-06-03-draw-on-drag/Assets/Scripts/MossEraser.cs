5615eda6c735f045c44e8a357ed69813cc34deab
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-06-03 14:36:39 +0200

    wip: moss eraser

---
+++ process/code/unity/2026-06-03-draw-on-drag/Assets/Scripts/MossEraser.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MossEraser : MonoBehaviour
+{
+    [Header("Layer des prefabs effaçables")]
+    public LayerMask erasableLayer;
+
+    [Header("Rayon d'effacement")]
+    public float eraseRadius = 0.3f;
+
+    private Camera _cam;
+
+    void Awake()
+    {
+        _cam = Camera.main;
+    }
+
+    void Update()
+    {
+        var mouse = Mouse.current;
+        if (mouse == null)
+            return;
+
+        if (mouse.leftButton.isPressed)
+            TryErase(mouse.position.ReadValue());
+    }
+
+    private void TryErase(Vector2 screenPos)
+    {
+        Ray ray = _cam.ScreenPointToRay(screenPos);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, erasableLayer))
+            return;
+
+        // Efface tous les prefabs dans le rayon autour du point frappé
+        Collider[] colliders = Physics.OverlapSphere(hit.point, eraseRadius, erasableLayer);
+        foreach (Collider col in colliders)
+            Destroy(col.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, eraseRadius);
+    }
+}
