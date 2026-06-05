a3344dcac40d925990149acdbc653fdf2faa66b6
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-06-05 16:00:59 +0200

    feat: leaf fall

---
+++ code/Let It Lie/Assets/Scripts/LeafSpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LeafSpawner : MonoBehaviour
+{
+    public GameObject leafPrefab;
+    public int maxLeaves = 100;
+
+    private int count = 0;
+
+    void Update()
+    {
+        if (count >= maxLeaves)
+            return;
+
+        if (Random.value < 0.05f) // ~3 feuilles/sec
+        {
+            Vector3 pos =
+                transform.position + new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f));
+            var leaf = Instantiate(leafPrefab, pos, Random.rotation);
+            float s = Random.Range(0.5f, 1.5f);
+            leaf.transform.localScale = leafPrefab.transform.localScale * s;
+            count++;
+        }
+    }
+}
