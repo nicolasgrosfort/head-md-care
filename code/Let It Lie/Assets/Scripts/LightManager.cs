e84f1ad36640ea17da17e9546a7999d9c8769661
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-06-08 16:06:36 +0200

    feat: day cicle

--- code/Let It Lie/Assets/Scripts/LightManager.cs
+++ code/Let It Lie/Assets/Scripts/LightManager.cs
@@ -4,22 +4,23 @@ public class LightManager : MonoBehaviour
 {
     [SerializeField]
     private GameState gameState;
+    private Light directionalLight;
 
-    public Color nightColor = new Color(0.05f, 0.05f, 0.15f); // 0
-    public Color dayColor = new Color(0.8f, 0.4f, 0.2f); // 1
+    public Color nightColor = new Color(0.05f, 0.05f, 0.15f);
+    public Color dayColor = new Color(0.8f, 0.4f, 0.2f);
 
     public float nightIntensity = 0.2f;
     public float dayIntensity = 1f;
 
-    void Start() { }
+    void Start()
+    {
+        directionalLight = GetComponent<Light>();
+    }
 
     void Update()
     {
-        float t = gameState.time;
-        Color color = Color.Lerp(nightColor, dayColor, t);
-        float intensity = Mathf.Lerp(nightIntensity, dayIntensity, t);
-
-        RenderSettings.ambientLight = color;
-        RenderSettings.ambientIntensity = intensity;
+        float t = Mathf.Sin(gameState.time * Mathf.PI);
+        directionalLight.color = Color.Lerp(nightColor, dayColor, t);
+        directionalLight.intensity = Mathf.Lerp(nightIntensity, dayIntensity, t);
     }
 }
