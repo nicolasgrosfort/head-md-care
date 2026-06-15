8353f20c27689a8959279d6948e668cc7f1be1e3
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-06-15 15:35:51 +0200

    feat: add star manager

--- code/Let It Lie/Assets/Scripts/StarManager.cs
+++ code/Let It Lie/Assets/Scripts/StarManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class StarManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Game State")]
+    [SerializeField]
+    private GameState gameState;
+
+    private ParticleSystem.EmissionModule emission;
+
+    private void Awake()
     {
-        
+        emission = GetComponent<ParticleSystem>().emission;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        gameState.OnTimeChange += HandleStars;
+    }
+
+    private void OnDisable()
+    {
+        gameState.OnTimeChange -= HandleStars;
+    }
+
+    private void HandleStars(float time)
+    {
+        float distanceToMidday = Mathf.Abs(time - 0.5f) * 2f;
+        float alpha = Mathf.SmoothStep(0f, 1f, distanceToMidday);
+        float rateOverTime = Mathf.Lerp(0f, 100f, alpha);
+        emission.rateOverTime = rateOverTime;
     }
 }
